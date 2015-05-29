72e99976e47166ffa9291052b0eabb86e16ebd3c
chandan-hm <chanhiremath2aug96@gmail.com>
2015-05-29 17:17:15 +0530

    Running Office
    ieeesummerofcode
    www.hackerrank.com/contests/ieeecontest1

---
+++ runningoffice.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t;
+    cin>>t;
+    while(t--)
+        {
+        int n,x1,x2;
+        cin>>n>>x1>>x2;
+        string z;
+        for(int i=0;i<(2*n);i++)
+            {
+            cin>>z;
+    }
+        
+        cout<<(x2-x1)<<endl;
+    }
+    return 0;
+}
